2d9325b9726023632124e195438fd3acd74edd75
kuhakuni <afif.haidar@student.ub.ac.id>
2022-07-25 23:23:43 +0700

    add homepage and stuffs

--- resources/views/layouts/web/navbar.blade.php
+++ resources/views/layouts/web/navbar.blade.php
@@ -10,11 +10,11 @@
     </button>
     <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
       <div class="navbar-nav ms-auto me-4 gap-3">
-        <a class="nav-link" aria-current="page" href="{{ url('beranda', []) }}">Beranda</a>
-        <a class="nav-link" href="{{ url('profil', []) }}">Profil</a>
-        <a class="nav-link" href="{{ url('berita', []) }}">Berita</a>
-        <a class="nav-link" href="{{ url('pariwisata', []) }}">Pariwisata</a>
-        <a class="nav-link" href="{{ url('umkm', []) }}">UMKM</a>
+        <a class="nav-link {{ $route === '/' ? "active" : "" }}" href="{{ url('/') }}">Beranda</a>
+        <a class="nav-link {{ $route === 'profil' ? "active" : "" }}" href="{{ url('/profil') }}">Profil</a>
+        <a class="nav-link {{ $route === 'berita' ? "active" : "" }}" href="{{ url('/berita') }}">Berita</a>
+        <a class="nav-link {{ $route === 'pariwisata' ? "active" : "" }}" href="{{ url('/pariwisata') }}">Pariwisata</a>
+        <a class="nav-link {{ $route === 'umkm' ? "active" : "" }}" href="{{ url('/umkm') }}">UMKM</a>
       </div>
     </div>
   </div>
